df7be4ba52c40b5a2ba7d14c7deee46f4ea58bb9
Prakash Thapa <prakashthap617@gmail.com>
2022-04-30 14:01:57 +0545

    model test

--- studyApp/models.py
+++ studyApp/models.py
@@ -1,3 +1,34 @@
+from pyexpat import model
+
 from django.db import models
 
 # Create your models here.
+SCHOOL_LEVEL = (
+    ("Order Received", "Order Received"),
+    ("Order Processing", "Order Processing"),
+    ("On the way", "On the way"),
+    ("Order Completed", "Order Completed"),
+    ("Order Canceled", "Order Canceled"),
+)
+
+BACHELOR_LEVEL = (
+    ("Order Received", "Order Received"),
+    ("Order Processing", "Order Processing"),
+    ("On the way", "On the way"),
+    ("Order Completed", "Order Completed"),
+    ("Order Canceled", "Order Canceled"),
+)
+
+MASTER_LEVEL = (
+    ("Order Received", "Order Received"),
+    ("Order Processing", "Order Processing"),
+    ("On the way", "On the way"),
+    ("Order Completed", "Order Completed"),
+    ("Order Canceled", "Order Canceled"),
+)
+
+class School(model.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
