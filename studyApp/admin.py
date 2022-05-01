d2f32299660b372310fb3eb2eab259ebc0bcbd90
Prakash Thapa <prakashthap617@gmail.com>
2022-05-01 13:33:15 +0545

    class in model added and template added..

--- studyApp/admin.py
+++ studyApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+from django.apps import apps
+
+app = apps.get_app_config('studyApp')
+
+for model_name, model in app.models.items():
+    admin.site.register(model)
+
+# admin.site.register([School])
--- studyApp/models.py
+++ studyApp/models.py
@@ -1,3 +1,152 @@
 from django.db import models
 
 # Create your models here.
+SCHOOL_LEVEL = (
+    ("one", "one"),
+    ("two", "two"),
+    ("Four", "Four"),
+    ("Five", "Five"),
+    ("Six", "Six"),
+    ("Seven", "Seven"),
+    ("Eight", "Eight"),
+    ("Nine", "Nine"),
+    ("Ten", "Ten"),
+    ("Eleven", "Eleven"),
+    ("Twelve", "Twelve"),
+  
+)
+
+BACHELOR_LEVEL = (
+    ("Order Received", "Order Received"),
+    ("Order Processing", "Order Processing"),
+    ("On the way", "On the way"),
+    ("Order Completed", "Order Completed"),
+    ("Order Canceled", "Order Canceled"),
+)
+
+MASTER_LEVEL = (
+    ("Order Received", "Order Received"),
+    ("Order Processing", "Order Processing"),
+    ("On the way", "On the way"),
+    ("Order Completed", "Order Completed"),
+    ("Order Canceled", "Order Canceled"),
+)
+
+class School(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+    
+class Bachelor(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+    
+class Master(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Faculty(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Semister(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Subjects(models.Model):
+    
+    School_subjects = models.ForeignKey(School, on_delete=models.SET_NULL, null=True, blank=True)
+    # Collage_subjects = models.CharField(max_length=100)
+    # Master_subjects = models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return self.School_subjects
+class Syllabus(models.Model):
+    Syllabus = models.OneToOneField(Subjects,on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+        return self.Syllabus
+class Master(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class QuestionBank(models.Model):
+    questionBank = models.OneToOneField(Subjects,on_delete=models.SET_NULL, null=True, blank=True)
+
+    
+    def __str__(self):
+        return self.questionBank
+class Notes(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Official_Note(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Chapter(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Topic(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Discriptions(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Table(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Note_Video(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Diagram(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Official_Comments(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Other_Notes(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Files(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Comments(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+class Other_Notes(models.Model):
+    level = models.CharField(max_length=50, choices= SCHOOL_LEVEL)
+    
+    def __str__(self):
+        return self.level
+    
